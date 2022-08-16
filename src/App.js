1dc2ac83562577daff56868e5f3ffa471452410a
hanzheng <zheng.han@fenbeitong.com>
2022-08-16 21:04:33 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { observer } from 'mobx-react';
+import {useState} from 'react'
 
-function App() {
+// isBad ------- begin -----------------------------------------
+const App = function () {
+
+  // toggle comments this line, the console will log error
+  // const [test, setTest] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +29,35 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
+// isBad ------- end -----------------------------------------
+
+
+// isGood -------- begin ---------------------------------------
+// const App = observer(() => {
+
+//   // toggle comments this line, isOk
+//   // const [test, setTest] = useState(0);
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// })
+
+// export default App
+// isGood -------- end ----------------------------------------
